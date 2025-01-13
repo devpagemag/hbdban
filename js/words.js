4bbd6061ebff28d3740a354ffe1f5a07b2f22718
devpagemag <61438266+devpagemag@users.noreply.github.com>
2025-01-14 00:05:18 +0800

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,63 @@
+let wordList = [
+    {
+        word: "banjo",
+        hint: "your nickname"
+    },
+    {
+        word: "bartu",
+        hint: "your AI alter ego"
+    },
+    {
+        word: "porcupine",
+        hint: "you if you are an animal- spiky"
+    },
+    {
+        word: "intj",
+        hint: "your mbti"
+    },
+    {
+        word: "january",
+        hint: "your birthmonth"
+    },
+    {
+        word: "balsomo",
+        hint: "your last name"
+    },
+    {
+        word: "yerd",
+        hint: "a term we call for yb 2015 staff"
+    },
+    {
+        word: "tupv",
+        hint: "your university"
+    },
+    {
+        word: "bsme",
+        hint: "your course"
+    },
+    {
+        word: "sweets",
+        hint: "kind of foods you don't really like"
+    },
+    {
+        word: "nonchalant",
+        hint: "opposite of oa"
+    },
+    {
+        word: "sarcasm",
+        hint: "/s"
+    },
+    {
+        word: "antagonist",
+        hint: "amo ka ni kung gusto ko manupo"
+    },
+    {
+        word: "canada",
+        hint: "country where you migrated"
+    },
+    {
+        word: "32",
+        hint: "your age now"
+    },
+    
+]
